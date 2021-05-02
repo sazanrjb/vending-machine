a990e11ae82d5c251537baafac1328cb0e60212d
Sajan Rajbhandari <sazanrjb@gmail.com>
2021-05-02 23:09:12 +0545

    Replace trans helper function with class

--- app/VendingMachine/Amount/Manager.php
+++ app/VendingMachine/Amount/Manager.php
@@ -5,6 +5,7 @@ namespace App\VendingMachine\Amount;
 use App\VendingMachine\Amount\Exceptions\AmountNotFoundException;
 use App\VendingMachine\Amount\Models\Amount;
 use App\VendingMachine\Core\ValueObjects\AmountType;
+use Illuminate\Support\Facades\Lang;
 
 class Manager
 {
@@ -25,11 +26,11 @@ class Manager
             ->first();
     }
 
-    public function addAmount(float $amount, int $amountType = AmountType::COIN)
+    public function addAmount(float $amount, int $amountType = AmountType::COIN): Amount
     {
         $amountModel = $this->findByType($amountType);
 
-        $this->createOrUpdate($amount, $amountType, $amountModel);
+        return $this->createOrUpdate($amount, $amountType, $amountModel);
     }
 
     public function createOrUpdate(float $amount, int $amountType, Amount $amountModel = null)
@@ -44,18 +45,20 @@ class Manager
         return $amountModel;
     }
 
-    public function deductAmount(float $amount, int $amountType = AmountType::COIN)
+    public function deductAmount(float $amount, int $amountType = AmountType::COIN): Amount
     {
         $amountModel = $this->findByType($amountType);
 
         throw_if(!$amountModel, new AmountNotFoundException(
-            trans('general.not_found', [
-                'Entity' => 'amount'
+            Lang::get('general.not_found', [
+                'Entity' => 'Amount'
             ])
         ));
 
         $amountModel->forceFill([
             'amount' => $amountModel->amount - $amount,
         ])->save();
+
+        return $amountModel;
     }
 }
