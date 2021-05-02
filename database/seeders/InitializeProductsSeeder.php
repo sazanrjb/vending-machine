4b4ecac00e0993a0dec1a41066fe5cc723fbd287
Sajan Rajbhandari <sazanrjb@gmail.com>
2021-05-02 14:44:56 +0545

    Create database seeder for products

---
+++ database/seeders/InitializeProductsSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\VendingMachine\Product\Models\Product;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Database\Seeder;
+
+class InitializeProductsSeeder extends Seeder
+{
+    const PRODUCTS = [
+        [
+            'name' => 'Coke',
+            'price' => 20,
+            'total' => 10
+        ],
+        [
+            'name' => 'Pepsi',
+            'price' => 25,
+            'total' => 10
+        ],
+        [
+            'name' => 'Dew',
+            'price' => 30,
+            'total' => 10
+        ]
+    ];
+
+    /**
+     * @var Product
+     */
+    private Product $product;
+    /**
+     * @var DatabaseManager
+     */
+    private DatabaseManager $databaseManager;
+
+    public function __construct(Product $product, DatabaseManager $databaseManager)
+    {
+        $this->product = $product;
+        $this->databaseManager = $databaseManager;
+    }
+
+    public function run()
+    {
+        $this->databaseManager->table('products')->truncate();
+
+        collect(self::PRODUCTS)->each(function (array $product) {
+            $this->product->newInstance()
+                ->forceFill([
+                    'name' => $product['name'],
+                    'price' => $product['price'],
+                    'total' => $product['total']
+                ])
+                ->save();
+        });
+    }
+}
